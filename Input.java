36e23532f06323e91bdc49f321ef4ef658f29aab
Sushovan <sushovanghosh960@gmail.com>
2024-04-11 14:25:18 +0530

    fix: add try to wrap Scanner method

--- Input.java
+++ Input.java
@@ -2,13 +2,14 @@ import java.util.*;
 
 public class Input {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String greetings = sc.next();
-        String name = sc.next();
+        try (Scanner sc = new Scanner(System.in)) {
+            String greetings = sc.next();
+            String name = sc.next();
 
-        int age = sc.nextInt();
-        float height = sc.nextFloat();
+            int age = sc.nextInt();
+            float height = sc.nextFloat();
 
-        System.out.println(greetings + ", " + name + ". I am " + height + " tall and " + age + " years old.");
+            System.out.println(greetings + ", " + name + ". I am " + height + " tall and " + age + " years old.");
+        }
     }
 }
